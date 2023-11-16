ab68690c7faac6f45e5de19b3cf661bab7b03b92
WazowskiMike <wazowskipuffs@yandex.ru>
2023-11-16 23:53:04 +0300

    delete exceptions

--- OOP-ICT.Third/CantBeNullException.cs
+++
@@ -1,8 +0,0 @@
-namespace OOP_ICT.Third;
-[Serializable]
-public class CantBeNullException : Exception
-{
-    public CantBeNullException() { }
-    public CantBeNullException(string message) : base(message) { }
-    public CantBeNullException(string message, Exception inner) : base(message) {}
-}
--- OOP-ICT.Third/WrongFormatException.cs
+++
@@ -1,9 +0,0 @@
-namespace OOP_ICT.Third;
-[Serializable]
-
-public class WrongFormatException : Exception
-{
-    public WrongFormatException() { }
-    public WrongFormatException(string message) : base(message) { }
-    public WrongFormatException(string message, Exception inner) : base(message) {}
-}
