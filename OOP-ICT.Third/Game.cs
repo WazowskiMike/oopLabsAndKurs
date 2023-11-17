cb87ce566a916eec981242659f8183de1cbc4455
dnslmnv <leeden18@mail.ru>
2023-11-17 14:35:12 +0300

    Made AddPlayers() and Start() and Game()

--- OOP-ICT.Third/Game.cs
+++ OOP-ICT.Third/Game.cs
@@ -1,15 +1,33 @@
+using System.Runtime.ConstrainedExecution;
 using OOP_ICT.Models;
 using OOP_ICT.Second;
 namespace OOP_ICT.Third;
 
 public class Game {
-    public static DealerAdapter dealerForThisGame;
+    private List<Player> playerList = new List<Player>();
+    private int chipPrice;
+
     public Game() {
-        dealerForThisGame = new DealerAdapter(new CardDeck());
-        dealerForThisGame.Shuffle();
+        CardDeck cardDeck = new CardDeck();
+        DealerAdapter dealer = new DealerAdapter(cardDeck);
+        
+        
     }
 
-    public void AddPlayers(string name, double deposit) {
-        
+    public void Start() {
+        Console.WriteLine("Пожалуйста, введите цену одной фишки ");
+        chipPrice = int.Parse(Console.ReadLine());
+        AddPlayers();
+
+    }
+
+    public void AddPlayers() {
+        while (Console.ReadLine().Trim() != ""){
+            Console.WriteLine("Пожалуйста, введите имя игрока ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Пожалуйста, введите баланс");
+            double deposit = Double.Parse(Console.ReadLine());
+            playerList.Add(new Player(name, deposit));
+        }
     }
 }
